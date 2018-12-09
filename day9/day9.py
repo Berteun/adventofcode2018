090639c7ac9eaf04560ae4bacf0ee8c2c91ba1eb
Berteun Damman <berteun@gmail.com>
2018-12-09 20:52:27 +0100

    small fix for day9

--- day9/day9.py
+++ day9/day9.py
@@ -1,19 +1,18 @@
 from collections import defaultdict, deque
 def read_input():
-    f = open("input.txt")
-    return [int(t) for (i,t) in enumerate(f.readline().split()) if i in (0, 6)]
+    return [int(t) for t in open("input.txt").readline().split()[0::6]]
 
 def play_game(players, rounds):
     scores = defaultdict(int)
     marbles = deque([0])
 
     for cur_round in range(1, rounds + 1):
-        if (cur_round % 23 == 0):
-            marbles.rotate(7)
-            scores[cur_round % players] += cur_round + marbles.popleft()
-        else:
+        if (cur_round % 23):
             marbles.rotate(-2)
             marbles.appendleft(cur_round)
+        else:
+            marbles.rotate(7)
+            scores[cur_round % players] += cur_round + marbles.popleft()
 
         #print "[{}/{}]".format(cur_round, cur_round % players), "({})".format(marbles[0]), marbles
 
