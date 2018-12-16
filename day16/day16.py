a8f7b08a53c0d76081c9c998d0718faf68e46a9d
Berteun Damman <berteun@gmail.com>
2018-12-16 17:50:38 +0100

    Code golfing day 16

--- day16/day16.py
+++ day16/day16.py
@@ -1,130 +1,56 @@
 import operator
-from dataclasses import dataclass
-from typing import List
-import re
+from collections import namedtuple
 
-@dataclass
-class Sample:
-    before: List[int]
-    after: List[int]
-    opcode: List[int]
+Sample = namedtuple('Sample', ['before', 'after', 'opcode'])
 
-def make_register(func):
-    def generic_r(A, B, C, registers):
-        registers[C] = int(func(registers[A], registers[B]))
-    return generic_r
+def do(combiner, operator, A, B, C, registers):
+    registers[C] = combiner(operator, A, B, registers)
+    return registers
 
-def make_immediate(func):
-    def generic_i(A, B, C, registers):
-        registers[C] = int(func(registers[A], B))
-    return generic_i
-
-def make_immediate_reverse(func):
-    def generic_ir(A, B, C, registers):
-        registers[C] = int(func(A, registers[B]))
-    return generic_ir
+rr = lambda *args : do(lambda op, A, B, R : int(op(R[A], R[B])), *args)
+ri = lambda *args : do(lambda op, A, B, R : int(op(R[A],   B )), *args)
+ir = lambda *args : do(lambda op, A, B, R : int(op(  A , R[B])), *args)
 
 opcodes = {
-    'addr' : make_register(operator.add),
-    'addi' : make_immediate(operator.add),
-    'mulr' : make_register(operator.mul),
-    'muli' : make_immediate(operator.mul),
-    'banr' : make_register(operator.and_),
-    'bani' : make_immediate(operator.and_),
-    'borr' : make_register(operator.or_),
-    'bori' : make_immediate(operator.or_),
-    'setr' : make_register(lambda A, B : A),
-    'seti' : make_immediate_reverse(lambda A, B : A),
-    'gtir' : make_immediate_reverse(operator.gt),
-    'gtri' : make_immediate(operator.gt),
-    'gtrr' : make_register(operator.gt),
-    'eqir' : make_immediate_reverse(operator.eq),
-    'eqri' : make_immediate(operator.eq),
-    'eqrr' : make_register(operator.eq),
+    'addr' : lambda *args : rr(operator.add  , *args) , 'addi' : lambda *args : ri(operator.add  , *args),
+    'mulr' : lambda *args : rr(operator.mul  , *args) , 'muli' : lambda *args : ri(operator.mul  , *args),
+    'banr' : lambda *args : rr(operator.and_ , *args) , 'bani' : lambda *args : ri(operator.and_ , *args),
+    'borr' : lambda *args : rr(operator.or_  , *args) , 'bori' : lambda *args : ri(operator.or_  , *args),
+    'setr' : lambda *args : rr(lambda A,B : A, *args) , 'seti' : lambda *args : ir(lambda A,B : A, *args),
+    'gtir' : lambda *args : ir(operator.gt   , *args) , 'gtri' : lambda *args : ri(operator.gt   , *args), 'gtrr' : lambda *args : rr(operator.gt, *args),
+    'eqir' : lambda *args : ir(operator.eq   , *args) , 'eqri' : lambda *args : ri(operator.eq   , *args), 'eqrr' : lambda *args : rr(operator.eq, *args),
 }
 
-def read_samples():
-    lines = open("input.txt").readlines()
-    offset = 0
-    samples = []
-    while True:
-        sample = lines[offset:offset + 4]
-        if not sample[0].startswith('Before'):
-            break
-        before_ = re.match(r'Before: \[(\d+), (\d+), (\d+), (\d+)\]', sample[0])
-        before = [int(n) for n in before_.group(1,2,3,4)]
-        opcode = [int(n) for n in sample[1].split()]
-        after_ = re.match(r'After:  \[(\d+), (\d+), (\d+), (\d+)\]', sample[2])
-        after = [int(n) for n in after_.group(1,2,3,4)]
-        samples.append(Sample(before=before, after=after, opcode=opcode))
-        offset += 4
-    return samples
-
-def read_program():
-    lines = open("input.txt").readlines()
-    offset = 0
-    while True:
-        if lines[offset].startswith('Before'):
-            offset += 4
-            continue
-        else:
-            offset += 1
-            if not lines[offset].strip():
-                continue
-            return [tuple(map(int, l.split())) for l in lines[offset:]]
+def read_input():
+    part1, part2 = open("input.txt").read().split("\n\n\n\n")
+    samples = [Sample(before=[int(n) for n in s.split("\n")[0][9:-1].split(', ')],
+               opcode=[int(n) for n in s.split("\n")[1].split()],
+               after=[int(n) for n in s.split("\n")[2][9:-1].split(', ')])
+               for s in part1.split("\n\n")]
+    program = [tuple(map(int, l.split())) for l in part2.split("\n") if l.strip()]
+    return samples, program
 
 def count_three_or_more(samples):
-    count = 0
-    for s in samples:
-        possible = 0
-        for op in opcodes:
-            registers = s.before[:]
-            A,B,C = tuple(s.opcode[1:])
-            opcodes[op](A,B,C, registers)
-            if registers == s.after:
-                possible += 1
-        if possible > 2:
-            count += 1
-    return count
+    return sum(sum(s.after == opcodes[op](*s.opcode[1:], s.before[:]) for op in opcodes) > 2 for s in samples)
 
 def make_opcodes_map(samples):
-    names = set(opcodes.keys())
-    candidates = {
-        n : names.copy() for n in range(len(opcodes))
-    }
+    return { s.opcode[0] : set(op for op,f in opcodes.items() if f(*s.opcode[1:], s.before[:]) == s.after) for s in samples }
 
-    for s in samples:
-        possible = set()
-        for op in opcodes:
-            registers = s.before[:]
-            code,A,B,C = s.opcode
-            opcodes[op](A,B,C, registers)
-            if registers == s.after:
-                possible.add(op)
-        candidates[code] &= possible
-
-    return candidates
-
-def reduce_map(opcode_map):
+def resolve_map(opcode_map):
     while any(len(l) > 1 for l in opcode_map.values()):
-        fixed = set(code for code in opcode_map if len(opcode_map[code]) == 1)
-        for n in fixed:
-            name = opcode_map[n].copy().pop()
-            for candidates in opcode_map.values():
-                if len(candidates) > 1 and name in candidates:
-                    candidates.remove(name)
+        for single, name in ((c,s.copy().pop()) for c,s in opcode_map.items() if len(s) == 1):
+            for multiple in (c for c in opcode_map.values() if len(c) > 1):
+                    multiple.discard(name)
     return { k : opcode_map[k].pop() for k in opcode_map }
 
 def evaluate(program, opm, registers):
-    for (code, A, B, C) in program:
-        opcodes[opm[code]](A, B, C, registers)
+    sum(len(opcodes[opm[code]](A, B, C, registers)) for (code, A, B, C) in program)
     return registers
 
 def run():
-    samples = read_samples()
+    samples,program = read_input()
     print(count_three_or_more((samples)))
-    m = reduce_map(make_opcodes_map(samples))
-    print(evaluate(read_program(), m, [0,0,0,0])[0])
+    print(evaluate(program, resolve_map(make_opcodes_map(samples)), [0,0,0,0])[0])
 
 if __name__ == '__main__':
     run()
